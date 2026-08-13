2031cda18e55dad7f034a9e4153f70a140605ccf
TheTurtleMelon <theturtlemelon@hotmail.com>
2026-08-13 16:53:18 +0100

    test
    test

---
+++ Assets/Scripts/Classes/Mod.cs
@@ -0,0 +1,13 @@
+using Thor.Core;
+using UnityEngine;
+
+namespace Mod_35b6f284f4f14a74831dde938ee66cbd
+{
+    public class Mod : MonoBehaviour
+    {
+        private void Awake()
+        {
+            // If your Mod needs to run code on startup, put it here and add this component to Mod.prefab!
+        }
+    }
+}
